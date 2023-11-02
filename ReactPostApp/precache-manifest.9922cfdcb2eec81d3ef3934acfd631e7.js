42d9b0baa964f799f6bf206123f6dc6f5a760bf4
DaniilPolskov <113332182+DaniilPolskov@users.noreply.github.com>
2023-11-02 10:46:16 +0200

    added react projects

---
+++ ReactPostApp/precache-manifest.9922cfdcb2eec81d3ef3934acfd631e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c0ff441c95c39c92255a",
+    "url": "/static/css/main.063b63e8.chunk.css"
+  },
+  {
+    "revision": "c0ff441c95c39c92255a",
+    "url": "/static/js/main.c0ff441c.chunk.js"
+  },
+  {
+    "revision": "66b140a9d814657d4f23",
+    "url": "/static/css/1.caa6e98e.chunk.css"
+  },
+  {
+    "revision": "66b140a9d814657d4f23",
+    "url": "/static/js/1.66b140a9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3542187b76fc0616dd61b08aa664242a",
+    "url": "/index.html"
+  }
+];
